b9dff23cf4fa7edab1045615ec9be3c66981e5dd
Felix Wruck <felix.wruck@aisec.fraunhofer.de>
2019-06-27 17:27:21 +0200

    Busybox compatibility of copy_image_to_disk.sh

--- yocto/copy_image_to_disk.sh
+++ yocto/copy_image_to_disk.sh
@@ -2,13 +2,14 @@
 
 INFILE=$1
 OUTFILE=$2
-PARTNUM=4
+PARTNUM=2
 
 case "$(basename $OUTFILE)" in
 	sd*	) INFIX="";;	
 	hd*	) INFIX="";;
 	nvm*	) INFIX="p";;
 	mmc*	) INFIX="p";;
+	loop*	) INFIX="p";;
 	*	)
 		echo "Unknown target device type. Exiting..."
 		exit;;
@@ -21,7 +22,14 @@ THIS WILL ERASE ALL DATA ON $OUTFILE [y/n]" FORMAT
 
 if [ "$FORMAT" = "y" ]; then
 	echo "overriding $OUTFILE as requested."
-	dd status=progress if=$INFILE of=$OUTFILE
+	dd if=$INFILE of=$OUTFILE bs=4096 &
+	DDPID=$!
+
+	while [ $(kill -USR1 $DDPID) -ne 0 ];do
+		sleep 1
+	done
+
+	sync
 	sgdisk --move-second-header $OUTFILE
 	partprobe
 else
@@ -29,19 +37,23 @@ else
 	exit
 fi
 
-sgdisk --largest-new=4 $OUTFILE
-
-sfdisk --part-label $OUTFILE $PARTNUM "containers"
-
-partprobe
-
 for i in $(seq 1 $PARTNUM); do
 	sfdisk --part-uuid $OUTFILE $i $(uuidgen)
+	sync
 	partprobe
 done
 
+SGDISK_FIRST_SECTOR="$(/sbin/sgdisk --info=2 "$OUTFILE$INFIX$PARTNUM" | grep 'First sector: [0-9]\+ .*' | awk -F ': ' '{print $2}' | awk -F ' ' '{print $1}')"
+SGDISK_END_OF_LARGEST="$(/sbin/sgdisk --end-of-largest)"
 
+sgdisk --delete=$PARTNUM
+sync
+partprobe
 
-PARTFILE="$OUTFILE$INFIX$PARTNUM"
+sgdisk "--new=$PARTNUM:${SGDISK_FIRST_SECTOR}s:${SGDISK_END_OF_LARGEST}s"
+sync
+partprobe
 
-mkfs.btrfs --force $PARTFILE
+resize2fs "$OUTFILE$INFIX$PARTNUM"
+sync
+partprobe
