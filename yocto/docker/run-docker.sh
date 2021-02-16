fa158ddc065acf20a6cfd337e52d99da6841cf90
Felix Wruck <felix.wruck@aisec.fraunhofer.de>
2021-02-16 18:46:46 +0100

    Updated Dockerfile + script for build container
    Current Linux distributions have versions of repotool
    older than 2 in their repositories.
    Those versions do not work properly with the current zeus build.
    
    Therefore, this commit updates the Dockerfile to Debian Buster
    and installs a current version of repotool from buster-backports.
    
    Also, the follwing changes have been made:
    	- Installation of a current JDK (11) available in Debian Buster
    	- Ensure usage of Python 2.7 for repo tool to work properly
    	- Create home directory for user 'builder'
    	- Add Dockerfile argument to pass user ID to be used
    	- Added argument to run-docker.sh to map ssh-agent socket to container
    
    Signed-off-by: Felix Wruck <felix.wruck@aisec.fraunhofer.de>

--- yocto/docker/run-docker.sh
+++ yocto/docker/run-docker.sh
@@ -6,8 +6,16 @@ if [ ! "$1" ]; then
 	exit
 fi
 
+EXTRA_ARGS=""
+
+if [ "$2" ];then
+	echo "Mapping ssh-agent to container"
+	EXTRA_ARGS="--volume $2:/tmp/sshagent --env=SSH_AUTH_SOCK=/tmp/sshagent"
+fi
+
 docker run \
  -it \
+${EXTRA_ARGS} \
  -e LOCAL_USER_ID=`id -u $USER` \
  -v "$1"/:/opt/ws-yocto/ \
  trustx-builder \
