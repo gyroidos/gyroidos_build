gyroidos/gyroidos_build

commit f012abe397a186809d86eee5062b08f067b73550

device_provisioning: cleanup of PKI generation scripts
Removed deprecated AOSP-specific parts of PKI-generation scripts.
Further, use variable names called GYROIDOS* instead of TRUSTME*.
Replaced all remaining trustme related references by GyroidOS-spcific
ones.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

device_provisioning/oss_enrollment/provisioning_lib.sh
@@ -1,22 +1,0 @@
-error_check(){
-if [ "$1" != "0" ]; then
-  echo "Error: $2"
-  cleanup
-  exit 1
-fi
-}
-
-assert_file_exists(){
-if [ ! -f $1 ]; then 
-  echo "Error: Missing file $1"
-  cleanup
-  exit 1
-fi
-}
-
-assert_file_not_exists(){
-if [ -f $1 ]; then 
-  echo "Error: File $1 exists. Precautional exit"
-  exit 1
-fi
-}

device_provisioning/gen_dev_certs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This file is part of trust|me
+# This file is part of GyroidOS
 # Copyright(c) 2013 - 2017 Fraunhofer AISEC
 # Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
 #
@@ -19,7 +19,7 @@
 # the file called "COPYING".
 #
 # Contact Information:
-# Fraunhofer AISEC <trustme@aisec.fraunhofer.de>
+# Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
 #
 
 set -e
@@ -44,14 +44,9 @@ mkdir ${OUT_CERTS_DIR}
 ##############################################
 ########## Software Signing PKI ##############
 
-if [ ! -z ${ANDROID_BUILD} ]; then
-	bash ${CERTS_DIR}/ssig_pki_generator.sh -p ${SELF_DIR}/test_passwd_env.bash
-	bash ${CERTS_DIR}/ssig_aosp_release_keys.sh -p ${SELF_DIR}/test_passwd_env.bash
-else
-	bash ${CERTS_DIR}/ssig_pki_generator.sh
-	if [ "${DO_PLATFORM_KEYS}" == "y" ]; then
-		bash ${CERTS_DIR}/sec_platform_keys.sh --dbkey ssig_subca
-	fi
+bash ${CERTS_DIR}/ssig_pki_generator.sh
+if [ "${DO_PLATFORM_KEYS}" == "y" ]; then
+	bash ${CERTS_DIR}/sec_platform_keys.sh --dbkey ssig_subca
 fi
 
 
@@ -72,12 +67,6 @@ fi
 bash ${CERTS_DIR}/gen_pki_generator.sh -p ${SELF_DIR}/test_passwd_env.bash
 bash ${CERTS_DIR}/gen_pki_backend_certs.sh -p ${SELF_DIR}/test_passwd_env.bash
 bash ${CERTS_DIR}/gen_ocsp_certs.sh -p ${SELF_DIR}/test_passwd_env.bash
-
-if [ ! -z ${ANDROID_BUILD} ]; then
-# generate user token and adbkey
-	bash ${CERTS_DIR}/usertoken_generator.sh -u dev.user -p ${SELF_DIR}/test_passwd_env.bash
-	mv ${CERTS_DIR}/dev.user.* ${OUT_CERTS_DIR}
-fi
 
 # copy generated test certificate and keys to out dir
 for i in cert key; do

device_provisioning/oss_enrollment/certificates/certificate_configs/openssl-backend-subca.cnf
@@ -46,7 +46,7 @@ organizationalUnitName		= Organizational Unit Name (eg, section)
 organizationalUnitName_default	= Development
 
 commonName          = Common Name (e.g. server FQDN or YOUR name)
-commonName_default      = trustme Backend Sub CA
+commonName_default      = GyroidOS Backend Sub CA
 
 ####################################################################
 [ ca_extensions ]

device_provisioning/oss_enrollment/certificates/certificate_configs/openssl-backend.cnf
@@ -21,7 +21,7 @@ organizationalUnitName	= Organizational Unit Name (department, division)
 organizationalUnitName_default 	= Development
 
 commonName          = Common Name (e.g. server FQDN or YOUR name)
-commonName_default      = trustme Backend Certificate
+commonName_default      = GyroidOS Backend Certificate
 
 ####################################################################
 [ user_req_extensions ]

device_provisioning/oss_enrollment/certificates/certificate_configs/openssl-device-subca.cnf
@@ -46,7 +46,7 @@ organizationalUnitName		= Organizational Unit Name (eg, section)
 organizationalUnitName_default	= Development
 
 commonName          = Common Name (e.g. server FQDN or YOUR name)
-commonName_default      = trustme Device Sub CA
+commonName_default      = GyroidOS Device Sub CA
 
 ####################################################################
 [ ca_extensions ]

device_provisioning/oss_enrollment/certificates/certificate_configs/openssl-gen-rootca.cnf
@@ -46,7 +46,7 @@ organizationalUnitName		= Organizational Unit Name (eg, section)
 organizationalUnitName_default	= Development
 
 commonName          = Common Name (e.g. server FQDN or YOUR name)
-commonName_default      = trustme General Root CA
+commonName_default      = GyroidOS General Root CA
 
 ####################################################################
 [ ca_extensions ]

device_provisioning/oss_enrollment/certificates/certificate_configs/openssl-ocsp-server.cnf
@@ -21,7 +21,7 @@ organizationalUnitName	= Organizational Unit Name (department, division)
 organizationalUnitName_default 	= Development
 
 commonName          = Common Name (e.g. server FQDN or YOUR name)
-commonName_default      = trustme OCSP Server Certificate
+commonName_default      = GyroidOS OCSP Server Certificate
 
 ####################################################################
 [ user_req_extensions ]

device_provisioning/oss_enrollment/certificates/certificate_configs/openssl-ssig-cml.cnf
@@ -21,7 +21,7 @@ organizationalUnitName	= Organizational Unit Name (department, division)
 organizationalUnitName_default 	= Development
 
 commonName          = Common Name (e.g. server FQDN or YOUR name)
-commonName_default      = trustme CML Signing Certificate
+commonName_default      = GyroidOS CML Signing Certificate
 
 ####################################################################
 [ user_req_extensions ]

commit ad227498e8926113f1079074fc817bbfad96e06d

device_provisioning/oss_enrollment: readd provisioning_lib.sh
Readd accidentally deleted provisioning_lib.sh. This is needed
for error checking and assertions in pki generation scripts.

Fixes: f012abe397a1 ("device_provisioning: cleanup of PKI generation scripts")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

device_provisioning/oss_enrollment/provisioning_lib.sh
@@ -1,0 +1,22 @@
+error_check(){
+if [ "$1" != "0" ]; then
+  echo "Error: $2"
+  cleanup
+  exit 1
+fi
+}
+
+assert_file_exists(){
+if [ ! -f $1 ]; then 
+  echo "Error: Missing file $1"
+  cleanup
+  exit 1
+fi
+}
+
+assert_file_not_exists(){
+if [ -f $1 ]; then 
+  echo "Error: File $1 exists. Precautional exit"
+  exit 1
+fi
+}