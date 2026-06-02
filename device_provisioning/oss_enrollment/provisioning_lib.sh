cee534c633f516b577b1cbe5f6fbc16ec4c2ab9a
Johannes Wiesböck <johannes.wiesboeck@aisec.fraunhofer.de>
2026-06-02 16:09:32 +0200

    device_provisioning/oss_enrollment/provisioning_lib: Algorithm parser
    Add a function that parses the user-provided algorithm specification and
    sets variables for openssl cli arguments accordingly. This is
    required for RSA and ECDSA ciphers that require special parameters. All
    other key types are passed through to openssl as-is (e.g. ed25519,
    ML-DSA-44, SLH-DSA-SHA2-128s).
    
    Signed-off-by: Johannes Wiesböck <johannes.wiesboeck@aisec.fraunhofer.de>

--- device_provisioning/oss_enrollment/provisioning_lib.sh
+++ device_provisioning/oss_enrollment/provisioning_lib.sh
@@ -22,3 +22,22 @@ if [ -f "$1" ]; then
   exit 1
 fi
 }
+
+set_newkey_args(){
+local key_type=${1}
+if [[ "${key_type}" =~ ^rsa:([0-9]+)$ ]]; then
+	local key_size="${BASH_REMATCH[1]}"
+	NEWKEY_ARGS_primary=(-newkey "rsa:${key_size}")
+	NEWKEY_ARGS_secondary=(-newkey "rsa-pss" -pkeyopt "rsa_keygen_bits:${key_size}")
+	SIGOPT_ARGS=(-sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:-1)
+elif [[ "${key_type}" =~ ^ec:(.+)$ ]]; then
+	local curve="${BASH_REMATCH[1]}"
+	NEWKEY_ARGS_primary=(-newkey ec -pkeyopt "ec_paramgen_curve:${curve}")
+	NEWKEY_ARGS_secondary=("${NEWKEY_ARGS_primary[@]}")
+	SIGOPT_ARGS=()
+else
+	NEWKEY_ARGS_primary=(-newkey "${key_type}")
+	NEWKEY_ARGS_secondary=("${NEWKEY_ARGS_primary[@]}")
+	SIGOPT_ARGS=()
+fi
+}
